31021923e84713a4f06a41983aeedf0bb0681d0e
NStiner <ndstiner@fullsail.edu>
2014-12-01 17:26:48 -0500

    adding changes to wpf files
    adding changes to wpf files

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -1,1 +1,11 @@
-alert("This is in the js file");
+// JavaScript
+
+var yearBorn = 1955;
+
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+        */
+
+alert("Hello World!");
+
--- AssociatingJS/declaring variables/script.js
+++
@@ -1,7 +0,0 @@
-// Defining Variables
-
-var age; //declaration
-age = 20; //declaration
-//storage container = value
-
-var yearOfBirth = 1994; //declaring and defining on the same line
