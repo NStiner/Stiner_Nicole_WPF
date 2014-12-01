0812c23d86b5cab33e7f9e8f9a8b2ac6317c3281
NStiner <ndstiner@fullsail.edu>
2014-12-01 18:52:08 -0500

    using different variables for the same action
    using different variables for the same action

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -3,4 +3,9 @@
 var muppetNames = ["Kermit" , "Miss Piggy" , "Gonzo", "Rizzo", 38]; //0,1,2,3
 
 muppetNames[1] = "Rizzo"; // change indes #1 to Rizzo
-console.log(muppetNames[1]); //lets me see whats in the index #1
+muppetNames[2] = "Fozzy";
+
+var num = 2;
+
+
+console.log(muppetNames[num]); //lets me see whats in the index #1
