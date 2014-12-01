5f39fabea0cd66c8620f542e49560b66e7206bf2
NStiner <ndstiner@fullsail.edu>
2014-12-01 18:43:33 -0500

    changing the index numbers of arrays
    changing the index numbers of arrays

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -2,4 +2,5 @@
 
 var muppetNames = ["Kermit" , "Miss Piggy" , "Gonzo", "Rizzo", 38]; //0,1,2,3
 
-console.log(muppetNames[1]);
+muppetNames[1] = "Rizzo"; // change indes #1 to Rizzo
+console.log(muppetNames[1]); //lets me see whats in the index #1
