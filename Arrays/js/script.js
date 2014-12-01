7ed141e59187399895ff20a3cd0738eb726795ce
NStiner <ndstiner@fullsail.edu>
2014-12-01 18:53:01 -0500

    Giving me Rizzo due to the variable directions
    Giving me Rizzo due to the variable directions. i.e num = 2

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -5,7 +5,7 @@ var muppetNames = ["Kermit" , "Miss Piggy" , "Gonzo", "Rizzo", 38]; //0,1,2,3
 muppetNames[1] = "Rizzo"; // change indes #1 to Rizzo
 muppetNames[2] = "Fozzy";
 
-var num = 2;
+var num = 1;
 
 
-console.log(muppetNames[num]); //lets me see whats in the index #1
+console.log(muppetNames[1]); //lets me see whats in the index #1
