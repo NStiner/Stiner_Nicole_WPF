982cda085b4bae627452fdfe68ce0f96d918d0d9
NStiner <ndstiner@fullsail.edu>
2014-12-02 18:14:09 -0500

    Completed code showing the console
    Completed code showing the console to enter height and width to
    calculate the area.

--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -1,4 +1,7 @@
 //Prompt and Alert
 
 var width = prompt("We are calculating the area of a rectangle. \n Please enter the width");
-var height = prompt("Please enter the height.")
+var height = prompt("Please enter the height.");
+var area = width * height; // calculates with that info
+console.log(area); //prints it out to console
+
