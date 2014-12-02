663e7438bb5c676b34cc39dfa8b2dea6c008bf0b
NStiner <ndstiner@fullsail.edu>
2014-12-01 21:33:48 -0500

    Treating stringVar as a number
    Treating stringVar as a number

--- Expressions - Casting/js/script.js
+++ Expressions - Casting/js/script.js
@@ -1,6 +1,6 @@
-//Casting
+//Casting - treating strings like numbers
 
 var stringVar = "6;"
-var result = 7 + stringVar;
+var result = 7 + Number(stringVar); //convert stringVar into a number
 console.log(result);
 
