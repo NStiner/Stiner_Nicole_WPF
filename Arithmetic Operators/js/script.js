a8fdcab2d1d9894885f117a646eec921e7ccabb2
NStiner <ndstiner@fullsail.edu>
2014-12-01 20:53:53 -0500

    Dividing the height and width
    Dividing the height and width

--- Arithmetic Operators/js/script.js
+++ Arithmetic Operators/js/script.js
@@ -3,6 +3,5 @@
 // area of the triangle is half of the width times the height
 var width = 4;
 var height = 5;
-var area = width * height;
-area = area/2;
+var area = width * height/2;
 console.log(area);
