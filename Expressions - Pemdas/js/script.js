16518e44bcde2ef7dc7fa41abfa6d3dcbac8e5c3
NStiner <ndstiner@fullsail.edu>
2014-12-01 21:06:17 -0500

    Adding the average
    Adding the average

--- Expressions - Pemdas/js/script.js
+++ Expressions - Pemdas/js/script.js
@@ -1,3 +1,10 @@
-/**
- * Created by nicolestiner on 12/1/14.
- */
+//Expressions - PEMDAS - The Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz4;
+console.log(average);
