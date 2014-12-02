61dc5c7383b89244622f02742540e415f51c16cb
NStiner <ndstiner@fullsail.edu>
2014-12-01 20:55:29 -0500

    Completed Code
    Completed Code

--- Arithmetic Operators/js/script.js
+++ Arithmetic Operators/js/script.js
@@ -1,7 +1,7 @@
 //Expressions - Arithmetic Operators
 
 // area of the triangle is half of the width times the height
-var width = 4;
-var height = 5;
+var width = 7;
+var height = 8;
 var area = width * height/2;
 console.log(area);
