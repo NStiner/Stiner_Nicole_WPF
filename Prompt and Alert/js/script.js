46ccb7a7e4507652cd426bb1011a844803c66e9d
NStiner <ndstiner@fullsail.edu>
2014-12-02 18:15:41 -0500

    using an alert to calculate the area.
    using an alert to calculate the area.

--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -3,5 +3,6 @@
 var width = prompt("We are calculating the area of a rectangle. \n Please enter the width");
 var height = prompt("Please enter the height.");
 var area = width * height; // calculates with that info
-console.log(area); //prints it out to console
+//console.log(area); //prints it out to console
 
+alert("The area of your rectangle is + area + sq ft ");
