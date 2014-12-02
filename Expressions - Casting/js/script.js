cd597deb024b91901975d30a55b951c31a63d863
NStiner <ndstiner@fullsail.edu>
2014-12-01 21:41:18 -0500

    Making correct format display
    Making correct format display by adding parenthesis and a hyphen

--- Expressions - Casting/js/script.js
+++ Expressions - Casting/js/script.js
@@ -3,7 +3,7 @@
 var areaCode = 407;
 var firstPart = 203;
 var secPart = 4529;
-
-var phoneNo = String(areaCode) + String(firstPart) + String(SecPart);
+//(407)203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(SecPart);
 console.log(phoneNo);
 
