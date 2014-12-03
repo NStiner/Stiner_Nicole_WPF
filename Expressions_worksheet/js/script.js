21ad32783c71243e54935c57ec052ad6e654b274
NStiner <ndstiner@fullsail.edu>
2014-12-03 16:23:44 -0500

    Ran a test
    Ran a test to make sure computer was doing what it was told -
    calculated dog years correctly.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,6 @@
 //Calculate how old sparky the pit bull is in dog years
 
 // Sparkys age in dog years is 7 times the age in human years
-var humanYears = x;
-var dogYears = x * 7;
-console.log(dogYears);
+var humanYears = 2; // putting in a number to run a test
+var dogYears = 2 * 7; // telling the computer to calculate sparkys age in dog years by multiplying by 7
+console.log(dogYears); // running the test
