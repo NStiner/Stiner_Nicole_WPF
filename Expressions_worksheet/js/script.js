f90d1350ceed9be1e5a402a22e59e72ae150d1d2
NStiner <ndstiner@fullsail.edu>
2014-12-03 18:11:34 -0500

    Completed Slice of Pie part1
    Completed Slice of Pie part1 - calculated how many slices the
    party-goers ate at the party.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,9 @@
-//Calculate how old sparky the pit bull is in dog years
+//Calculate how much pizza will each party-goer will get at the party
 
-// Sparkys age in dog years is 7 times the age in human years
-var humanYears = 2; // putting in a number to run a test
-var dogYears = 2 * 7; // telling the computer to calculate sparkys age in dog years by multiplying by 7
-console.log(dogYears); // running the test
+var slicesPerPizza = 8;
+var numberOfPeople = 23;
+var orderedPizzas = 10;
+//multipy pizzaOrdered and SlicesPerPizza then divide the result by numberOfPeople
+var slicesPerPerson = (orderedPizzas * slicesPerPizza) / numberOfPeople;
+//printing out the result for slicesPerPerson
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
