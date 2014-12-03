6ec5196c263d28c8044476cc94d0235b6d2e6091
NStiner <ndstiner@fullsail.edu>
2014-12-03 18:18:16 -0500

    Completed Sparkys age
    Completed Sparkys age by calculating the age in dog years from human
    years.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,3 +1,13 @@
+//Calculate sparkys age in dog years
+var sparkysAge = 2;
+var dogAge = sparkysAge * 7;
+//multiply sparkysAge and 7(dogyears) to calculate sparkys age
+console.log(" Sparky is " +  sparkysAge  + " human years old which is  " +  dogAge  + "in dog years.");
+//printing our the result for sparkys age.
+
+
+
+
 //Calculate how much pizza will each party-goer will get at the party
 
 var slicesPerPizza = 8;
