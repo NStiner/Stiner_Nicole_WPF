0a567d2fd3d7a30f9e98c0f82630d4a86d40624b
NStiner <ndstiner@fullsail.edu>
2014-12-03 16:20:39 -0500

    Commanding the computer to calculate Sparkys age
    Commanding the computer to calculate Sparkys age

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,2 +1,6 @@
 //Calculate how old sparky the pit bull is in dog years
 
+// Sparkys age in dog years is 7 times the age in human years
+var humanYears = x;
+var dogYears = x * 7;
+console.log(dogYears);
--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -5,4 +5,4 @@ var height = prompt("Please enter the height.");
 var area = width * height; // calculates with that info
 //console.log(area); //prints it out to console
 
-alert("The area of your rectangle is + area ");
+alert("The area of your rectangle is" + area );
