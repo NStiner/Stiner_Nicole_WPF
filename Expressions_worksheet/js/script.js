b1bdedd5b7fe753ee219b3201b657f0e235aab9a
NStiner <ndstiner@fullsail.edu>
2014-12-03 21:43:31 -0500

    Completed Expressions Worksheet
    Completed Expressions Worksheet - completed all questions required for
    expressions worksheet

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -30,3 +30,36 @@ var orderedPizzas = 10;
 var sparkyFood = (orderedPizzas * slicesPerPizza) % numberOfPeople;
 console.log(" Sparky got "  +  sparkyFood  +  " slices of pizza.");
 //printing out the result for sparkys leftover amount
+
+
+
+
+// Calculate the average amount spent on groceries
+var spentOnGroceries = [89.65 , 112.04 , 63.74 , 92.12 , 105.22];
+//add each variable in spentOnGroceries
+var totalSpending = spentOnGroceries[0]; + spentOnGroceries[1]; + spentOnGroceries[2]; + spentOnGroceries[3]; + spentOnGroceries[4];
+//divide totalSpending by 5 (the amount of # in the array spentOnGroceries)
+var weeklyAverage = totalSpending / 5;
+//print out result - running test
+console.log(" You have spent a total of $ " + totalSpending + " on groceries over 5 weeks. That is an average of $ " + weeklyAverage + " per week " );
+
+
+
+
+
+//Calculate the discounted price with and without tax
+var originalPrice = 849.99;
+var discountPercent = 15;
+var descriptionOfItem = "55 inch smart led tv";
+var salesTaxPercentage = 6.75;
+//calculate the price with tax
+var discountNoTax = originalPrice * (100 - discountPercent) / 100;
+var discountTax = discountNoTax + (discountNoTax * salesTaxPercentage) / 100;
+//print out result
+console.log( " Your " + descriptionOfItem + " was originally $ " + originalPrice + " but after a " + discountPercent + " % discount, it is now $ " + discountNoTax + " without tax, and $ " + discountTax + " with tax. ");
+
+
+
+
+
+
