77f4e529d4dbbcac8a48c23f7fabca651bcf5bba
NStiner <ndstiner@fullsail.edu>
2014-12-03 22:12:18 -0500

    Entered in the variables to calculate the problem
    entered in the variable to calculate the problem

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,7 @@
-/**
- * Created by nicolestiner on 12/3/14.
- */
+//Calculate how much you spend on gas in a week.
+
+alert ("We are calculating the price you spend on gas per week."); // we are alerting (telling) the user what we plan on doing
+var pricePerGallon = prompt ("Enter the price per gallon"); // Tells the user to insert the price per gallon
+var milesWeek = prompt ("Enter the amount of miles you travel in a week"); // has the user enter the miles they drove that week
+var amount =  milesWeek * pricePerGallon; // this calculates how much you will spend per gallon within the week
+
