b0f87130157871936be8d49aa95e952d2b1cc5dc
NStiner <ndstiner@fullsail.edu>
2014-12-05 15:05:06 -0500

    Conditional notes, greater than etc
    Conditional notes, greater than, comparison variables, alternative
    choices

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -25,3 +25,23 @@ if(sunny){
 }else{
     GoToTheMovies();
 }
+
+
+//sometimes temperature doesnt allow you to go to beach to swim, so you must add more details
+if(temp > 70){
+    GoToTheBeach();
+}
+//use comparison to see if variable is true or false
+temp = 82;
+
+if(temp > 70){
+    GoToTheBeach();
+}
+//if temp is less than 70 degrees, then you wont go to the beach. If it is greater than, it will prompt you to go to the beach
+// ( > greater than ) ( < less than ) ( >= greather than or equal to) ( <= less than or equal to) ( != not equal to) ( = = equal to)
+
+if (temp == 75){
+    GoToTheBeach();
+}
+
+//two equal signs compares the variables
