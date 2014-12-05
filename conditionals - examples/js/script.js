41df5bdfa6ef5a1335de23cda5afb9194c5544cd
NStiner <ndstiner@fullsail.edu>
2014-12-05 15:33:10 -0500

    add if statements
    add if statements

--- conditionals - examples/js/script.js
+++ conditionals - examples/js/script.js
@@ -1,14 +1,16 @@
-//Conditional Logic - with an Expression
+//Conditional Logic - if and else
 
 
-var kidHeight = 40;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
-//kidHeight + 2 would be if his shoes add 2 inches
-if(kidHeight + sneakerLift > minHeight){
+
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first!");
 }
