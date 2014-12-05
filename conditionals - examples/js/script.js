3b2fb1f891f5f675b1958d86f4559fd3b8d5c1cb
NStiner <ndstiner@fullsail.edu>
2014-12-05 15:30:25 -0500

    Completed with expressions
    Completed with expressions

--- conditionals - examples/js/script.js
+++ conditionals - examples/js/script.js
@@ -3,11 +3,12 @@
 
 var kidHeight = 40;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 //kidHeight + 2 would be if his shoes add 2 inches
-if(kidHeight + 2 > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
