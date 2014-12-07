8df9cee6c822e9215f337faea9637c210342d2f7
NStiner <ndstiner@fullsail.edu>
2014-12-07 17:50:44 -0500

    Completed Movie Tickets
    Completed Movie Tickets having the user prompt its age and time in
    order to receive price for ticket

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -25,9 +25,22 @@ if( password >= "love789"){
 
 //Movie Tickets
 
-var regTicketPrice = 12.00;
-var discTicketPrice = 7.00;
-var matineeTime = 3-5;
+var regTicketPrice = 12.00; //creating variable for regular ticket price
+var discTicketPrice = 7.00; // creating variable for discount ticket price for matinee time and age 55 => or <=10
+var matineeTime = 3-5; // creating the matinee time frame
+
+var age = prompt("Enter your age"); // having the user enter in their age
+var matineeTime = prompt("Enter the time of the movie"); // having the user enter the time they plan to go
+
+if(age >= 55 || age <=10) { // setting an age limit for discount price
+        console.log("The ticket price is $" + discTicketPrice); // printing out result
+}else if(matineeTime >= 3 && matineeTime <= 5){ // setting up matinee price for any age
+                console.log( "The ticket price is " + discTicketPrice); // printing out result
+}else{
+        console.log("Your ticket price is $" + regTicketPrice); // if age and matinee failed, this is the result
+
+}
+
 
 
 
