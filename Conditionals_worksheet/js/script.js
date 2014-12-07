390f6811e11361211ec116e1e0ee8b5c1f2dce7a
NStiner <ndstiner@fullsail.edu>
2014-12-07 10:21:01 -0500

    creating username and password
    creating username and password prompts

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by nicolestiner on 12/5/14.
- */
+//Last Chance For Gas!
+
+
+
+
+
+
+//Check the Login
+prompt = ("Please enter your username.");
+var userName = "NStiner123";
+if(userName >= "NStiner123"){
+        prompt = ("Please type in your password.");
+}else if(userName != "NStiner123"){
+        console.log("User not found. Try again");
+}
+
+var password = "love789";
+if( password >= "love789"){
+        console.log("Welcome," + userName + "!");
+}else {
+        console.log("Password does not match our records.");
+}
+
