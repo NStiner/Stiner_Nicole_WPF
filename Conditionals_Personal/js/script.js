311844e87b9b8fc8a999c06230324ec27c0c3e89
NStiner <ndstiner@fullsail.edu>
2014-12-08 14:15:39 -0500

    Saving for a later time
    Saving for a later time - competed different variables and print outs
    for the different prompts entered in by the user

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,31 @@
-/**
- * Created by nicolestiner on 12/8/14.
- */
+//Choosing an activity by age and weather conditions
+//if the child is under 5, they get story time
+//if the age is between 5-10, they play games - if weather is <70 they play outside
+//ages 11-17 will play dodgeball if it rains - kickball if sunny
+//age 18 < arcade
+
+var ageEntered = prompt("Please enter you age.");
+
+if(ageEntered < 5){
+    console.log("You qualify for story time!");
+}else{
+    var tempEntered = prompt("Please enter the temperature outside");
+}
+
+if(ageEntered <= 10 <= 5 && tempEntered >= 70){
+        console.log("You get to play outside!");
+}else{
+        console.log("Sorry, due to the weather you must play indoors!")
+    }
+
+if(ageEntered >= 11 <= 17 && tempEntered >= 70){
+        console.log("You get to play dodgeball");
+} else if( tempEntered < 70) {
+    console.log("You get to play kickball!");
+}else {
+    console.log("You qualify for the arcade!");
+}
+
+
+
+
