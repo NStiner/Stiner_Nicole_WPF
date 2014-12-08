27b51f7209ce8e57f7a47e72026e6227543a59fe
NStiner <ndstiner@fullsail.edu>
2014-12-08 13:26:03 -0500

    Created conditionals assignment files
    Created conditionals assignment files and linked the html and script.js

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,28 +1,38 @@
-//Last Chance For Gas!
+//Celsius to Fahrenheit
+// c = (F-32)*.56  and  F = C * 1.8 + 32
+var temperature = prompt ("Please enter the temperature you wish to convert. \n Please be specific by using C for Celsius \n and F for Fahrenheit.");
+
+if( temperature = )
 
 
 
 
 
 
-/*//Check the Login
-prompt = ("Please enter your username.");
-var userName = "NStiner123";
-if(userName >= "NStiner123"){
-        prompt = ("Please type in your password.");
-}else if(userName != "NStiner123"){
+/*
+//Check the Login
+var correctUserName = "NStiner123";
+var correctPassword = "love789";
+
+var userNameEntered = prompt("Please enter your username.");
+
+if(userNameEntered >= "NStiner123") {
+        var passwordEntered = prompt("Please type in your password.");
+        if ("passwordEntered == correctPassword ")
+                console.log("Welcome," + correctUserName + "!");
+}else if("passwordEntered != correctPassword"){
+                console.log("Password does not match our records.");
+}else{
         console.log("User not found. Try again");
-}
-
-var password = "love789";
-if( password >= "love789"){
-        console.log("Welcome," + userName + "!");
-}else {
-        console.log("Password does not match our records.");
 }
 */
 
 
+
+
+
+
+/*
 //Movie Tickets
 
 var regTicketPrice = 12.00; //creating variable for regular ticket price
@@ -40,6 +50,8 @@ if(age >= 55 || age <=10) { // setting an age limit for discount price
         console.log("Your ticket price is $" + regTicketPrice); // if age and matinee failed, this is the result
 
 }
+*/
+
 
 
 
