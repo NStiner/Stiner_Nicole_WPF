5b38371ad163b52849fefd973b4d3bf9e5eb0cfe
NStiner <ndstiner@fullsail.edu>
2014-12-09 20:59:01 -0500

    creating variables and prompts for the user
    creating variables and prompts for the user by having them enter in
    their information for the questions asked.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,7 @@
+//calculating how many employees a store can have while still gaining a profit after paying for employees, items, products, etc
+var dailyAccumulation = prompt("Please enter the amount of money your business accumulates in 24 hours.");
+var employees = prompt("Please enter the amount of employees that work at your business.");
+var amountOfPaychecks = prompt("Please enter the total amount of your employees paychecks per week.");
+var totalCostForGoods = prompt("Please enter the total amount you spend on durable goods and items for your business.");
+
 
