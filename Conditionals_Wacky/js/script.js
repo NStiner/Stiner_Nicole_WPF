a096c85212df29160ef374eebbad9667cb976469
NStiner <ndstiner@fullsail.edu>
2014-12-10 18:10:07 -0500

    Starting to make prompts for color of shirts
    Starting to make prompts for color of shirts

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,10 @@
-//
+//guessing shirt and birth month say about ouy
+
+var shirt = prompt = ("Please enter the color of shirt you are wearing.");
+
+if(shirt == white){
+    console.log("You have a boring life!");
+}esle if(shirt == black){
+    console.log("You choose to feel powerful to")
+}
+
