aa6ab955f09e40300f5e6533b1e1b248636a6f3c
NStiner <ndstiner@fullsail.edu>
2014-12-10 18:35:03 -0500

    created expressions for all shirt colors
    created expressions for all shirt colors

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,10 +1,32 @@
 //guessing shirt and birth month say about ouy
 
+alert("Find out what your shirt color says about you!");
+
+var colors = ["white" , "black" , "red" , "pink" , "blue", "green" , "yellow" , "orange" , "purple" , "grey"];
+
 var shirt = prompt = ("Please enter the color of shirt you are wearing.");
 
-if(shirt == white){
-    console.log("You have a boring life!");
-}esle if(shirt == black){
-    console.log("You choose to feel powerful to")
+if(shirt == colors["white"]){
+    console.log("Your mood is centered! Nothing can ruin your simple day.");
+}else if(shirt == colors["black"]){
+    console.log("Your mood is dark and slum, and unsocial. You prefer dark over light.");
+}else if (shirt == colors["red"]){
+    console.log("Your mood is angry and bold! Be careful today. Small things will tick you off. ");
+}else if (shirt == colors["pink"]){
+    console.log("You mood is sweet and loving today! Be sure to share your cheer!");
+}else if (shirt == colors["blue"]){
+    console.log("Your feeling very calm and focused today! Make your focus take you that extra step to success! ");
+}else if (shirt == colors["green"]){
+    console.log("You are an Earth lover today! Take that extra step in recycling today.");
+}else if (shirt == colors["yellow"]){
+    console.log("Your mood is happiness! Shine bright, and send that smile to others!");
+}else if (shirt == colors["orange"]){
+    console.log("Your mood is alert and ambition! You are alert and ready for today's adventures.");
+}else if (shirt == colors["purple"]){
+    console.log("Your mood is wealthy and wisdom. Great wealth may be in your future today, or stumble across some great intelligence!");
+}else if (shirt == colors["grey"]){
+    console.log("You mood is completely neutral today! Your day consists with lack of energy, and confidence.");
+}else{
+    console.log("Your mood is 'unique'! You have unlimited abilities with your adventures today!");
 }
 
