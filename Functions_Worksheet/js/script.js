4950857f62fa69009bd19333f77a1be0f4dc50e7
NStiner <ndstiner@fullsail.edu>
2014-12-13 21:14:34 -0500

    testing one weight for bee stings
    testing one weight for bee stings

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by nicolestiner on 12/13/14.
- */
+// Stung - calculating how many bee stings it would take to kill an animal
+// takes 8.666666667 bee stings per pound to kill an animal
+// Given victims weight lbs Parameters victims weight lbs
+    // return number bee stings Print # bee stings
+
+function totalBeeStings(weight){
+    var totalBeeStings = weight * 8.666666667;
+    console.log("It takes " + totalBeeStings + " bee stings to kill this animal");
+}
+
+totalBeeStings(40);
