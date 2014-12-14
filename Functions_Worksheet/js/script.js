0569d5c373bd47f7235e3f9cca78e41913d516a9
NStiner <ndstiner@fullsail.edu>
2014-12-13 21:18:04 -0500

    Completed functions worksheet
    Completed functions worksheet

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,12 +1,11 @@
 // Stung - calculating how many bee stings it would take to kill an animal
 // takes 8.666666667 bee stings per pound to kill an animal
-// Given victims weight lbs Parameters victims weight lbs
-    // return number bee stings Print # bee stings
 
-function totalBeeStings(weight){
-    var totalBeeStings = weight * 8.666666667;
-    console.log("It takes " + totalBeeStings + " bee stings to kill this animal");
+
+function totalBeeStings(weight){ // calling the function totalBeeStings to connect with argument weight
+    var totalBeeStings = weight * 8.666666667; // creating parameters
+    console.log("It takes " + totalBeeStings + " bee stings to kill this animal"); // print out result
 }
 
-totalBeeStings(40);
-totalBeeStings(80);
+totalBeeStings(40); // test 1
+totalBeeStings(80); // test 2
