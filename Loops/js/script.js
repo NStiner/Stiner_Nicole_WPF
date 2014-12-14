97cad8a98aadce6da56175a134679f1c89823c5b
NStiner <ndstiner@fullsail.edu>
2014-12-13 21:29:46 -0500

    completed while loop
    completed while loop

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,8 +1,8 @@
-//loops
+//loops - while
 
-var b = 10;
+var b = 50; // sets up the index
 
-while (b > 0){
+while (b > 0){ // checks the condition
     console.log(b + " kegs on the wall");
-    b--;
+    b--; // increments or decrements the index
 }
