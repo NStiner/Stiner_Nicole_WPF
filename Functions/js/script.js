752dc34b0afe17a2b92743f404ec2a7911d6620e
NStiner <ndstiner@fullsail.edu>
2014-12-13 19:13:45 -0500

    shows an error
    invoking should happen after defining the variable

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,12 +1,12 @@
 //Functions - Anonymous Functions
 
+var a = calcArea(20, 30); // invoking
 
-var calcArea = function(width, height){
+var calcArea = function(width, height){ //defining
 //code the function runs
     var area = width * height;
     return area;
 }
 
-var a = calcArea(20, 30);
 
 console.log(a);
