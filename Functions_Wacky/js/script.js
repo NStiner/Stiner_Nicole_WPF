31dbfd3853ea7baf9dffdf555cdd8de52ee01089
NStiner <ndstiner@fullsail.edu>
2014-12-15 20:32:50 -0500

    calculating amount of gallons needed
    calculating amount of gallons needed

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by nicolestiner on 12/13/14.
- */
+//We are going to Calculate how many Gallons of water you need to fill your pool
+
+width = prompt("Please enter the width of your pool");
+height = prompt("Please enter the height of your pool");
+
+function calcGallons(width, height){
+    var gallonsNeeded = width * height;
+    return gallonsNeeded;
+}
+
+
+
+
