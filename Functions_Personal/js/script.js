d1c2f2bc60d17bf244364922aab19fd289da96f6
NStiner <ndstiner@fullsail.edu>
2014-12-17 19:32:37 -0500

    Calculating the amount of calories consumed
    Calculating the amount of calories consumed and presenting that
    information to the user.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,11 +5,14 @@ prompt1 = ("Please enter your gender. Use F for female and M for male.");
 prompt2 =("Please enter your age");
 prompt3 = ("Please enter how many calories you have consumed so far today");
 
+var caloriesConsumed = prompt3;
+var caloriesNeededPerDay = 1600;
+var caloriesNeeded = caloriesNeededPerDay - caloriesConsumed;
 
-if(prompt2 >=4 && prompt2 <= 8){
-    var caloriesConsumed = prompt3;
-    var caloriesNeededPerDay = 1600;
-    var caloriesNeeded = caloriesNeededPerDay - caloriesConsumed;
+if(prompt2 >=4 && prompt2 <= 8 && caloriesNeeded < caloriesNeededPerDay){
     console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}else if(prompt2 >=4 && prompt2 <=8 && caloriesNeeded >= caloriesNeededPerDay);
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}else{
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
 }
-if()
