fb98b6c694057e4318262a04c4be8ecf64d4c14a
NStiner <ndstiner@fullsail.edu>
2014-12-18 18:30:34 -0500

    almost completed Function personal
    almost completed Function personal  one error, still debugging

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,19 +1,54 @@
 //Track Your Diet! Count Your Calories!
 
 alert = ("We are going to track your diet!");
-prompt1 = ("Please enter your gender. Use F for female and M for male.");
-prompt2 =("Please enter your age");
-prompt3 = ("Please enter how many calories you have consumed so far today");
+prompt1 = prompt ("Please enter your gender. Use F for female and M for male.");
+prompt2 = prompt("Please enter your age");
+prompt3 = prompt("Please enter how many calories you have consumed so far today");
 
 var caloriesConsumed = prompt3;
 var caloriesNeededPerDay = 1600;
 var caloriesNeeded = caloriesNeededPerDay - caloriesConsumed;
 
-if(prompt2 >=4 && prompt2 <= 8 && caloriesNeeded < caloriesNeededPerDay){
+if(prompt2 >= 4 && prompt2 <= 8 && caloriesNeeded < caloriesNeededPerDay){
     console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
-}else if(prompt2 >=4 && prompt2 <=8 && caloriesNeeded >= caloriesNeededPerDay);
+}else if(prompt2 >= 4 && prompt2 <= 8 && caloriesNeeded >= caloriesNeededPerDay){
     console.log("You have consumed the correct amount of calories you need each day! Good Job!");
 }else{
     console.log("You have consumed the correct amount of calories you need each day! Good Job!");
 }
+
+
+
+if(prompt2 >= 9 && prompt2 <= 18 && caloriesNeeded < 2000){
+    console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}else if(prompt2 >= 9 && prompt2 <=18 && caloriesNeeded >= 2000){
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}else{
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}
+
+if(prompt2 >= 19 && prompt2 <= 30 && caloriesNeeded < 2200){
+    console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}else if(prompt2 >= 19 && prompt2 <=30 && caloriesNeeded >= 2000){
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}else{
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}
+
+if(prompt2 >= 31 && prompt2 <= 50 && caloriesNeeded < 2200){
+    console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}else if(prompt2 >= 31 && prompt2 <= 50 && caloriesNeeded >= 22000){
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}else{
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}
+
+if(prompt2 <= 51 && caloriesNeeded < 1800){
+    console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}else if(prompt2 >= 4 && prompt2 <= 30 && caloriesNeeded >= 1800){
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}else{
+    console.log("You have consumed the correct amount of calories you need each day! Good Job!");
+}
+
 
