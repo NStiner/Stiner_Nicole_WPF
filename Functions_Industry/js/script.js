b850913986f959cfc1999ea9d3d08369050da561
NStiner <ndstiner@fullsail.edu>
2014-12-17 19:09:54 -0500

    Creating Functions wacky
    calculating how much money you will need for a ski / snowboard trip

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,3 @@
-/**
- * Created by nicolestiner on 12/13/14.
- */
+//Calculate how many pills to give a customer due to the directions given by the doctor
+
+prompt = ("Please enter the amount of medicine you take per how many times per day. (Example - for directions take 1 tablet, 3 times daily, please enter 3 ")
--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,12 +1,14 @@
-//We are going to Calculate how many Gallons of water you need to fill your pool
+// Calculate how much money you will need for a ski / snowboard trip
 
-width = prompt("Please enter the width of your pool");
-height = prompt("Please enter the height of your pool");
+prompt1 = ("Please enter how many ski's you will need to rent. \n The price is $57 per day");
+prompt2 = ("Please enter how many ski's you will need to rent. \n The price is $69 per day");
 
-function calcGallons(width, height){
-    var gallonsNeeded = width * height;
-    return gallonsNeeded;
+function calcPrice(numberOfSkis, numberOfSnowboards){
+    var price = (numberOfSkis * 57) + (numberOfSnowboards * 69);
+    console.log(price);
+
 }
+
 
 
 
