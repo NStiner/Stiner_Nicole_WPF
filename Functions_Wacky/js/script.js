fec8f220106f11b9a8349a5b610b7eeb5e3f16a6
NStiner <ndstiner@fullsail.edu>
2014-12-18 17:29:16 -0500

    completed functions wacky
    completed functions wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,13 +1,15 @@
 // Calculate how much money you will need for a ski / snowboard trip
 
-prompt1 = ("Please enter how many ski's you will need to rent. \n The price is $57 per day");
-prompt2 = ("Please enter how many ski's you will need to rent. \n The price is $69 per day");
+var prompt1 = prompt("Please enter how many ski's you will need to rent. \n The price is $57 per day");
+var prompt2 = prompt("Please enter how many snowboards you will need to rent. \n The price is $69 per day");
+var results = calcPrice(prompt1, prompt2);
+console.log(results);
 
-function calcPrice(numberOfSkis, numberOfSnowboards){
+function calcPrice(numberOfSkis, numberOfSnowboards) {
     var price = (numberOfSkis * 57) + (numberOfSnowboards * 69);
-    console.log(price);
-
+    return price;
 }
+
 
 
 
