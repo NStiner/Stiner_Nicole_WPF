81883379a4895addd4fef94419456d5283355367
NStiner <ndstiner@fullsail.edu>
2014-12-17 19:24:38 -0500

    Starting a block of code for functions personal
    tracking diet - asking the user to enter personal information

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,15 @@
-/**
- * Created by nicolestiner on 12/13/14.
- */
+//Track Your Diet! Count Your Calories!
+
+alert = ("We are going to track your diet!");
+prompt1 = ("Please enter your gender. Use F for female and M for male.");
+prompt2 =("Please enter your age");
+prompt3 = ("Please enter how many calories you have consumed so far today");
+
+
+if(prompt2 >=4 && prompt2 <= 8){
+    var caloriesConsumed = prompt3;
+    var caloriesNeededPerDay = 1600;
+    var caloriesNeeded = caloriesNeededPerDay - caloriesConsumed;
+    console.log("You still need to consume " + caloriesNeeded + " calories in order to reach your daily needed amount!");
+}
+if()
