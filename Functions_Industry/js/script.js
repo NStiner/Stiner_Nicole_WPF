465eea014b8875bc7febf53cad2ce78c66878391
NStiner <ndstiner@fullsail.edu>
2014-12-18 19:52:47 -0500

    created blocks for age groups
    created blocks for age groups

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,38 @@
-//Calculate how many pills to give a customer due to the directions given by the doctor
+// Find the right gift!
 
-prompt = ("Please enter the amount of medicine you take per how many times per day. (Example - for directions take 1 tablet, 3 times daily, please enter 3 ")
+alert = ("Need Help finding the right gift? You came to te right place!");
+
+var age = prompt("Please enter the age of the person you are buying for.");
+
+if(age >= 1 && age <= 4) {
+    var gender = prompt("Please enter the gender of your child. Use F for female and M for male.");
+    result = (gender == F) ? "Your child might like an interactive baby doll." : "Your child might like a set of Hot Wheels and accessories like the race track.";
+    console.log(result);
+}
+
+if(age >= 5 && age <= 7) {
+    var gender = prompt("Please enter the gender of your child. Use F for female and M for male.");
+    result = (gender == F) ? "Your child might like the Barbie safe-drive four wheeler." : "Your child might like a hot Wheels safe-drive four wheeler.";
+    console.log(result);
+}
+
+if(age >= 8 && age <= 10) {
+    var gender = prompt("Please enter the gender of your child. Use F for female and M for male.");
+    result = (gender == F) ? "Your child might like Frozen items like clothes, dress up items, bath, etc." : "Your child might like video games for the console they own, or buy them a video game console \n(It teaches hand eye cordination!)";
+    console.log(result);
+}
+
+if(age >= 11 && age <= 13) {
+    var gender = prompt("Please enter the gender of your child. Use F for female and M for male.");
+    console.log("Your child might like a brand new bike!")
+}
+
+if(age >= 14 && age <= 17) {
+    var gender = prompt("Please enter the gender of your child. Use F for female and M for male.");
+    result = (gender == F) ? "Your child might like new clothes, purses, or makeup." : "Your child might like the new hot electronic, like an iPad or laptop.";
+    console.log(result);
+}
+
+if(age >= 18) {
+  console.log("Your gift receiver might be interested in an android wear")
+}
